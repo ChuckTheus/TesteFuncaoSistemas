bcd698d4b22ea2dba14c8220ba00c94f5959b6b1
Matheus Freire <matheus_freireoliveira@hotmail.com>
2025-03-29 18:11:05 -0300

    feat: criaçao de classes relacionadas ao beneficiario; ajuste nas procedures de clientes para incluir CPF

---
+++ FI.AtividadeEntrevista/BLL/BoBeneficiarios.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FI.AtividadeEntrevista.BLL
+{
+    public class BoBeneficiarios
+    {
+        /// <summary>
+        /// Inclui um novo beneficiario
+        /// </summary>
+        /// <param name="beneficiario">Objeto de beneficiario</param>
+        public long Incluir(DML.Beneficiario beneficiario)
+        {
+            DAL.DaoBeneficiario ben = new DAL.DaoBeneficiario();
+            return ben.Incluir(beneficiario);
+        }
+
+        /// <summary>
+        /// Altera um beneficiario
+        /// </summary>
+        /// <param name="beneficiario">Objeto de beneficiario</param>
+        public void Alterar(DML.Beneficiario beneficiario)
+        {
+            DAL.DaoBeneficiario ben = new DAL.DaoBeneficiario();
+            ben.Alterar(beneficiario);
+        }
+
+        /// <summary>
+        /// Excluir o beneficiario pelo id
+        /// </summary>
+        /// <param name="id">id do beneficiario</param>
+        /// <returns></returns>
+        public void Excluir(long id)
+        {
+            DAL.DaoBeneficiario ben = new DAL.DaoBeneficiario();
+            ben.Excluir(id);
+        }
+
+        /// <summary>
+        /// Lista os beneficiarios
+        /// </summary>
+        public List<DML.Beneficiario> Listar(long idCliente)
+        {
+            DAL.DaoBeneficiario ben = new DAL.DaoBeneficiario();
+            return ben.Listar(idCliente);
+        }
+
+        public bool VerificarExistencia(string CPF, long idCliente)
+        {
+            DAL.DaoBeneficiario ben = new DAL.DaoBeneficiario();
+            return ben.VerificarExistencia(CPF, idCliente);
+        }
+    }
+}
